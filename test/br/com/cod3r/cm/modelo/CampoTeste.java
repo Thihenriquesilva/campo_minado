9ecf57dafa0df9576a1e3c5137025fb5c32e42ed
Thiago <henriquesilva322@outlook.com>
2024-01-03 13:31:05 -0300

    Arquivos do projeto: Campo minado

---
+++ test/br/com/cod3r/cm/modelo/CampoTeste.java
@@ -0,0 +1,142 @@
+package br.com.cod3r.cm.modelo;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import br.com.cod3r.cm.excecao.ExplosaoException;
+
+public class CampoTeste {
+	private Campo campo ;
+	
+	@BeforeEach
+	void iniciarCampo() {
+		campo = new Campo(3,3);
+	}
+	
+	@Test
+	void testeViznhoRealDistancia1Esquerda() {
+		Campo vizinho = new Campo(3,2);
+		
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	
+	@Test
+	void testeViznhoRealDistancia1Direita() {
+		Campo vizinho = new Campo(3,4);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeViznhoRealDistancia1EmCima() {
+		Campo vizinho = new Campo(2,3);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeViznhoRealDistancia1Embaixo() {
+		Campo vizinho = new Campo(4,3);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeViznhoRealDistancia2() {
+		Campo vizinho = new Campo(2,2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testNaoVizinho() {
+		Campo vizinho = new Campo(1,1);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertFalse(resultado);
+	}
+	
+	@Test
+	void testeValorPadraoAtributoMarcado() {
+		assertFalse(campo.isMarcado());
+	}
+	
+	@Test
+	void testeAlternarMarcacao() {
+		campo.alternarMarcacao();
+		assertTrue(campo.isMarcado());
+	}
+	
+	
+	@Test
+	void testeAlternarMarcacaoDuasChamadas() {
+		campo.alternarMarcacao();
+		campo.alternarMarcacao();
+		assertFalse(campo.isMarcado());
+	}
+	
+	@Test
+	void testeAbrirNaoMinadoNaoMarcado() {
+		assertTrue(campo.abrir());
+	}
+	
+	@Test
+	void testeAbrirNaoMinadoMarcado() {
+		campo.alternarMarcacao();
+		assertFalse(campo.abrir());
+	}
+	
+	@Test
+	void testeAbrirMinadoMarcado() {
+		campo.alternarMarcacao();
+		campo.minar();
+		assertFalse(campo.abrir());
+	}
+	@Test
+	void testeAbrirMinadoNaoMarcado() {
+		campo.minar();
+		
+		//Testa o tipo de exceção gerada
+		//Testa se o método lança uma excessão
+		assertThrows(ExplosaoException.class, () ->{
+			campo.abrir();
+		});
+		
+		
+	}
+	
+	@Test
+	void testeAbrirComViznhos() {
+		Campo campo11 = new Campo(1,1);
+		Campo campo22 = new Campo(2,2);
+		
+		campo22.adicionarVizinho(campo11);
+		
+		campo.adicionarVizinho(campo22);
+		
+		campo.abrir();
+		
+		assertTrue(campo22.isAberto() && campo11.isAberto());
+	}
+	
+	@Test
+	void testeAbrirComViznhos2() {
+		Campo campo11 = new Campo(1,1);
+		Campo campo12 = new Campo(1,2);
+		campo12.minar();
+		Campo campo22 = new Campo(2,2);
+		
+		campo22.adicionarVizinho(campo11);
+		campo22.adicionarVizinho(campo12);
+		campo.adicionarVizinho(campo22);
+		
+		campo.abrir();
+		
+		assertTrue(campo22.isAberto() && campo11.isFechado());
+	}
+}
